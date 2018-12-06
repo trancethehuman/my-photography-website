8af0a4ec5272579910abd18f945777285b2a605d
Hai Nghiem <hainghiem375@gmail.com>
2018-12-06 18:49:44 -0330

    none

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,6 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=50)
-    body = models.CharField(max_length=2000)
+    body = models.TextField()
+    publicationDate = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
