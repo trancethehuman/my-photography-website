04bc8a762c4c46cccbdf7f4e3ea4402a48d05233
Hai Nghiem <hainghiem375@gmail.com>
2018-12-06 20:31:50 -0330

    delete blog

--- blog/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Blog(models.Model):
-    title = models.CharField(max_length=50)
-    body = models.TextField()
-    publicationDate = models.DateTimeField()
-    image = models.ImageField(upload_to='images/')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs.apps.JobsConfig',
-    'blog.apps.BlogConfig',
+
 ]
 
 MIDDLEWARE = [
